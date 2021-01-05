f4691ff13634f5fdf66c3605fd77c29124812c0c
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-01-05 17:37:52 +0100

    Connect to server script

---
+++ MadRunner Unity Project/Assets/Scripts/Client.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        print("Client connecting to Server...");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Client connected to Master Server!");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Client disconnected from Server. Cause: " + cause.ToString());
+    }
+}
