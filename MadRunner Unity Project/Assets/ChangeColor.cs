0bbf71863f1ba9cc869ed747927c10357499656d
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-01-06 18:07:30 +0100

    Trying out a color picker for the car, Right now it changes all meshes' materials to its color

---
+++ MadRunner Unity Project/Assets/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public FlexibleColorPicker cp;
+    public GameObject carGeometry;
+
+    public void ChangeCarColor()
+    {
+        int childCount = carGeometry.transform.childCount;
+
+        for(int i = 0; i < childCount; ++i)
+        {
+            GameObject go = carGeometry.transform.GetChild(i).gameObject;
+            Renderer r = go.GetComponent<Renderer>();
+
+            int range = r.materials.Length;
+
+            for (int j = 0; j < range; ++j)
+            {
+                r.materials[j].color = cp.color;
+            }
+        }
+    }
+}
