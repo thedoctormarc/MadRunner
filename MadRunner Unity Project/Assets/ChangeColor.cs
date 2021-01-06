12eda0b1f14afe80cfde1899402fdde4822dd9d0
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-01-06 20:10:52 +0100

    New car in color selection

--- MadRunner Unity Project/Assets/ChangeColor.cs
+++ MadRunner Unity Project/Assets/ChangeColor.cs
@@ -5,23 +5,18 @@ using UnityEngine;
 public class ChangeColor : MonoBehaviour
 {
     public FlexibleColorPicker cp;
-    public GameObject carGeometry;
 
     public void ChangeCarColor()
     {
-        int childCount = carGeometry.transform.childCount;
 
-        for(int i = 0; i < childCount; ++i)
+        Renderer r = gameObject.GetComponent<Renderer>();
+
+        int range = r.materials.Length;
+
+        for (int j = 0; j < range; ++j)
         {
-            GameObject go = carGeometry.transform.GetChild(i).gameObject;
-            Renderer r = go.GetComponent<Renderer>();
-
-            int range = r.materials.Length;
-
-            for (int j = 0; j < range; ++j)
-            {
-                r.materials[j].color = cp.color;
-            }
+            r.materials[j].color = cp.color;
         }
     }
+
 }
