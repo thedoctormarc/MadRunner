a132ea5fb70548a37975cf6da785b6bcedd2be68
vsRushy <vsRushy@users.noreply.github.com>
2021-01-07 23:48:06 +0100

    Instantiate Player Controller.

---
+++ MadRunner Unity Project/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        if(PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        Debug.Log("Player Controller Instantiated.");
+    }
+}
