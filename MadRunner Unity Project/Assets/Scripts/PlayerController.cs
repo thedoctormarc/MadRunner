8930d0a35054e28e7f7eb2b4f4cf4fc2bfb0ceae
vsRushy <vsRushy@users.noreply.github.com>
2021-01-08 12:48:31 +0100

    Fixed car as Photon prefab and testing movement.

--- MadRunner Unity Project/Assets/Scripts/PlayerController.cs
+++ MadRunner Unity Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    float mouse_sensitivity, sprint_speed, walk_speed, jump_force, smooth_time;
+
+    float vertical_look_rotation;
+    bool grounded;
+    Vector3 smooth_move_velocity;
+    Vector3 move_amount;
+
+    Rigidbody rb;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // TODO: This is for testing purposes only. Here we test the rotation of the car with the mouse.
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouse_sensitivity);
     }
 }
