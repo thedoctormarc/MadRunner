c0bca18d912c1114c3738a4d835a3e75cccc4cd0
vsRushy <vsRushy@users.noreply.github.com>
2021-01-10 00:18:27 +0100

    Lap system almost done.

---
+++ MadRunner Unity Project/Assets/Scripts/LapLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class LapLogic : MonoBehaviour
+{
+    PhotonView PV;
+
+    public int max_laps = 3;
+    public int current_lap = 1;
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            GameObject go_lap = GameObject.Find("Lap");
+            Text text = go_lap.GetComponent<Text>();
+            text.text = current_lap.ToString() + "/" + max_laps.ToString();
+        }
+    }
+}
---
+++ MadRunner Unity Project/Assets/Scripts/StartLineLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLineLogic : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject go = other.gameObject;
+        LapLogic ll = go.GetComponent<LapLogic>();
+
+        if (ll.current_lap < ll.max_laps)
+        {
+            ++ll.current_lap;
+        }
+    }
+}
