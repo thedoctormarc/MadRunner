fd690af5b312eda2fd6e0f8507cb291b7ad611a7
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-01-09 13:26:38 +0100

    Cars spawned in pole

--- MadRunner Unity Project/Assets/Scripts/PlayerManager.cs
+++ MadRunner Unity Project/Assets/Scripts/PlayerManager.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using System.IO;
+using System.Linq;  
 
 public class PlayerManager : MonoBehaviour
 {
     PhotonView PV;
+
+    [SerializeField]
+    Vector3 polePosition;
+
+    [SerializeField]
+    [Range(4.5f, 6f)]
+    float carDistanceX = 5f;
+
+    [SerializeField]
+    [Range(6.5f, 8f)]
+    float carDistanceZ = 7f;
 
     private void Awake()
     {
@@ -15,7 +28,7 @@ public class PlayerManager : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     public void Set(Color carColor)
@@ -30,10 +43,27 @@ public class PlayerManager : MonoBehaviour
     {
         Debug.Log("Player Controller Instantiated.");
 
-        object[] myCustomInitData = { carColor.r, carColor.g, carColor.b, carColor.a} ;
+        object[] myCustomInitData = { carColor.r, carColor.g, carColor.b, carColor.a };
 
-        Vector3 position = new Vector3(Random.Range(-20.0f, 20.0f), 0, Random.Range(-20.0f, 20.0f));
-        GameObject car = PhotonNetwork.Instantiate(Path.Combine("Photon Prefabs", "Car"), position, Quaternion.identity, 0, myCustomInitData);
+        int index = 0;
+
+        // We assume that photon player list is already disordered
+        for(int i = 0; i < PhotonNetwork.PlayerList.Length; ++i)
+        {
+            if(PhotonNetwork.PlayerList[i].NickName == PhotonNetwork.NickName)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        // z forward positive, x right positive
+        Vector3 position = polePosition;
+        position.z -= index * carDistanceZ;
+        position.x += (index % 2 == 0) ? 0f : carDistanceX;
+
+        PhotonNetwork.Instantiate(Path.Combine("Photon Prefabs", "Car"), position, Quaternion.identity, 0, myCustomInitData);
 
     }
+
 }
