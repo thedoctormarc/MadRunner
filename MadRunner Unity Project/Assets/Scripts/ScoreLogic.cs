8ec818912e63b5a93e54c595e3cf2688616e7780
vsRushy <vsRushy@users.noreply.github.com>
2021-01-09 22:31:54 +0100

    First timer approach.

---
+++ MadRunner Unity Project/Assets/Scripts/ScoreLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreLogic : MonoBehaviour
+{
+    Text text;
+    float total_time;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        total_time = 0.0f;
+    }
+
+    void Update()
+    {
+        total_time += Time.deltaTime;
+        text.text = total_time.ToString("#.00");
+    }
+}
