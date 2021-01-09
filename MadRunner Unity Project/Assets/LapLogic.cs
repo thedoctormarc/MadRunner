4598c76bbc2d1f673d611cfad6bace4e6b4bddab
vsRushy <vsRushy@users.noreply.github.com>
2021-01-09 23:30:36 +0100

    Starting to prepare lap system.

---
+++ MadRunner Unity Project/Assets/LapLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapLogic : MonoBehaviour
+{
+    public int max_laps = 3;
+    public int current_lap = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
