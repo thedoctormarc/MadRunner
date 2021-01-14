f732cfc2951171daad246544a21257401201a4b7
vsRushy <vsRushy@users.noreply.github.com>
2021-01-14 23:37:12 +0100

    Game can be paused by pressing ESC.

---
+++ MadRunner Unity Project/Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    bool is_paused = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(is_paused)
+                Time.timeScale = 1.0f;
+            else
+                Time.timeScale = 0.0f;
+
+            is_paused = !is_paused;
+        }
+    }
+}
