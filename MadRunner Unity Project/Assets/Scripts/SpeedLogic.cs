b1e6a10caddec76792d598bb52422d0c09d04f4f
vsRushy <vsRushy@users.noreply.github.com>
2021-01-14 21:01:42 +0100

    Displaying KM/H (needs to be adjusted still).

--- MadRunner Unity Project/Assets/Scripts/SpeedLogic.cs
+++ MadRunner Unity Project/Assets/Scripts/SpeedLogic.cs
@@ -7,17 +7,21 @@ public class SpeedLogic : MonoBehaviour
 {
     GameObject go_speed;
     Text text;
+    Rigidbody rb;
 
     void Start()
     {
         go_speed = GameObject.Find("Speed");
+
         text = go_speed.GetComponent<Text>();
-        text.text = transform.position.x + "Km/h";
+        rb = GetComponent<Rigidbody>();
+
+        text.text = (int)rb.velocity.magnitude + " Km/h";
     }
 
 
     void Update()
     {
-        text.text = transform.position.x + "Km/h";
+        text.text = (int)rb.velocity.magnitude + " Km/h";
     }
 }
