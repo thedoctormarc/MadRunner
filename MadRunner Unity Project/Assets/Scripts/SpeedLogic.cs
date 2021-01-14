143634dddf10204dd4ff4c0358ed4fac9ac10cd4
vsRushy <vsRushy@users.noreply.github.com>
2021-01-14 20:38:42 +0100

    Speed first test GUI.

---
+++ MadRunner Unity Project/Assets/Scripts/SpeedLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedLogic : MonoBehaviour
+{
+    GameObject go_speed;
+    Text text;
+
+    void Start()
+    {
+        go_speed = GameObject.Find("Speed");
+        text = go_speed.GetComponent<Text>();
+        text.text = transform.position.x + "Km/h";
+    }
+
+
+    void Update()
+    {
+        text.text = transform.position.x + "Km/h";
+    }
+}
