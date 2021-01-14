41d34d56829fe1e55143f224aae4e7934821b0e0
vsRushy <vsRushy@users.noreply.github.com>
2021-01-14 23:52:26 +0100

    Darken screen.

--- MadRunner Unity Project/Assets/Scripts/MenuController.cs
+++ MadRunner Unity Project/Assets/Scripts/MenuController.cs
@@ -1,26 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     bool is_paused = false;
 
+    Image dark;
+
     void Start()
     {
-        
+        dark = GetComponent<Image>();
     }
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            if(is_paused)
-                Time.timeScale = 1.0f;
-            else
-                Time.timeScale = 0.0f;
-
             is_paused = !is_paused;
+
+            if (is_paused)
+            {
+                Time.timeScale = 0.0f;
+                dark.enabled = true;
+            }
+            else
+            {
+                Time.timeScale = 1.0f;
+                dark.enabled = false;
+            }
         }
     }
 }
