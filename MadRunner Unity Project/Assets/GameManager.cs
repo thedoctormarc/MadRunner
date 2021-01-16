15cf2078b2e4da6f19847fc6398df128d7f3f555
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-01-16 18:43:21 +0100

    Render the other player's name on top of their cars WIP

---
+++ MadRunner Unity Project/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [HideInInspector]
+    public GameObject onwPlayer;
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
