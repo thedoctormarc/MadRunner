31b719248e463460cecef7158449b4c3fc5bfacc
vsRushy <vsRushy@users.noreply.github.com>
2021-01-27 00:03:57 +0100

    Showing player list on ending game.

--- MadRunner Unity Project/Assets/ShowResults.cs
+++ MadRunner Unity Project/Assets/ShowResults.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class ShowResults : MonoBehaviour
 {
+    public GameObject results_item_prefab;
+    public Transform results_item_transform;
+
     void Start()
     {
-        
+        foreach (Transform t in results_item_transform)
+        {
+            Destroy(t.gameObject);
+        }
+
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            Instantiate(results_item_prefab, results_item_transform).GetComponent<PlayerListItem>().Set(p);
+        }
     }
 
     void Update()
     {
-        
+
     }
 
     public void EnableChildren()
